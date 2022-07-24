82bce9ea765a40ede986cb5c3e4b9adb26fa6e56
Guilherme Soares <guilherme1808@hotmail.com>
2022-07-24 11:24:27 -0300

    primeiras aulas do curso, até aula 06.

---
+++ JS_2022/aula03/index.js
@@ -0,0 +1,9 @@
+console.log('Guilherme Soares');
+console.log("Guilherme 'Soares'");  //String = Texto
+console.log("Guilherme Soares");
+console.log('Guilehrme "Soares"');
+console.log(`Guilherme Soares`);
+console.log(`'Guilherme' "Soares"`);
+
+console.log(123456, 15645512.8554545121);   //Number = Número
+console.log(35, 15.85, 'Guilherme Soares De Novo');
